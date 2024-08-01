2cff9bd97788ba2732ced9a5388287b40e2371a9
Jewoo LEE <149163099+jewoooo@users.noreply.github.com>
2024-08-01 10:46:20 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240801/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -2,15 +2,19 @@
 #include <algorithm>
 using namespace std;
 
-void    FindN(int n, int m) {
-    int i = max(n, m);
+// 최대 공약수를 구한뒤 n * m / 최대공약수 = 최소공배수
 
-    while (true) {
-        if (i % n == 0 && i % m == 0)
-            break ;
-        i++;
+int    FindGcd(int n, int m) {
+    int gcd = 0;
+
+    for (int i = 1; i <= min(n, m); i++) {
+        if (n % i == 0 && m % i == 0)
+            gcd = i;
     }
-    cout << i;
+    return (gcd);
+}
+void    FindN(int n, int m) {
+    cout << n * m / FindGcd(n, m);
 }
 
 int main() {
